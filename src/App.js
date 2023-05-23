ff7865f25055a744de32567bde76799c8a534de9
chandiosamad07 <absamadchandio@gmail.com>
2023-05-23 10:08:40 +0500

    worked on props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import Car from './funcationcomponents/functionbaseCom';
-import Counter from './funcationcomponents/Counter';
-import Car1 from './classComponents/Car';
-import Values from './ContextAPI';
-import Welcome from './classComponents/welcome';
-import Greeting from './funcationcomponents/greeting';
+import "./App.css";
+import Car from "./funcationcomponents/functionbaseCom";
+import Counter from "./funcationcomponents/Counter";
+import Car1 from "./classComponents/Car";
+import Values from "./ContextAPI";
+import Welcome from "./classComponents/welcome";
+import Greeting from "./funcationcomponents/greeting";
+import Greetingprop from "./Props/Greetingprop";
 function App() {
   return (
     <div className="App">
-     <Car1 />
-    <withLoading/>
-    <Values/>
-  <Welcome></Welcome>
-  <Greeting></Greeting>
+      <Car1 />
+      <withLoading />
+      <Values />
+      <Welcome></Welcome>
+      <Greeting></Greeting>
+      <Greetingprop name="Abdul" />
+      <Greetingprop name="Samad" />
+      <Greetingprop name="Chandio" />
+      
     </div>
   );
 }
