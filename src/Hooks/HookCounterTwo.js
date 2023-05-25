e85af98a94eda272a943b61073b545e11c2a7da6
chandiosamad07 <absamadchandio@gmail.com>
2023-05-26 00:59:59 +0500

    counter using UseState hOOKS

--- src/Hooks/HookCounterTwo.js
+++ src/Hooks/HookCounterTwo.js
@@ -2,12 +2,19 @@ import React,{useState} from 'react'
 
 function HookCounterTwo() {
     const [count ,setCount] = useState(0)
+    
+    const incrementfive = () =>{
+        for(let i=0;i<5;i++){
+            setCount(prevCount => prevCount + 1)
+        }
+    }
   return (
     <div>
       Count : {count}
       <button onClick={()=>setCount(0)}>Reset</button>
       <button onClick={()=>setCount(count+1)}>Increment</button>
       <button onClick={()=>setCount(count-1)}>Decrement</button>
+      <button onClick={incrementfive}>Increment five</button>
     </div>
   )
 }
