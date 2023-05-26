1b3d7d546429db09c7580a7b4fa03d7006bf5e84
chandiosamad07 <absamadchandio@gmail.com>
2023-05-26 11:22:55 +0500

    Fecting data using Hooks

--- src/Hooks/DataFecting.js
+++ src/Hooks/DataFecting.js
@@ -3,25 +3,33 @@ import axios from 'axios'
 
 function DataFecting() {
 
-    const [post, setPost]= useState([])
-
+    const [post, setPost]= useState({})
+    const [id , setID] = useState(1)
+    const[idfromButton, setIdfromButton] = useState(1)
+ 
+    const handleClick =() =>{
+        setIdfromButton(id)
+    }
     useEffect(()=>{
-        axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idfromButton}`).then(res => {
             console.log(res)
             setPost(res.data)
         } )
         .catch(err =>{
             console.log(err)
-        })
+        },[id])
     })
 
   return (
     <div>
-      <ul>
+        <input type='text' value={id} onChange={e =>setID(e.target.value)}></input>
+        <button type='button' onClick={handleClick}>Fetch Post</button>
+        <div>{post.title}</div>
+      {/* <ul>
         {
             post.map(post => <li key={post.id}>{post.title}</li>)
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
